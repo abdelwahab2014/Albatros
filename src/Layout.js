c268abda677932d3ef4e95cc32c30c3dbb128d4d
Abdelwahab <data28448802@gmail.com>
2022-09-21 17:21:26 +0200

    wow444444

--- src/Layout.js
+++ src/Layout.js
@@ -467,7 +467,7 @@ const Layout = () => {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item" >
-          <a class="nav-link " aria-current="page" href="#home">g000HOME</a>
+          <a class="nav-link " aria-current="page" href="#home">HOME</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#services">SERVICES</a>
